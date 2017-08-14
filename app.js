23c8837b2689f62528f149056f6d315a7391712a
Your Name <you@example.com>
2017-08-14 00:12:18 -0400

    robot styling ok

--- app.js
+++ app.js
@@ -5,23 +5,26 @@ const data = require('./data.js');
 app.use(express.static('public'))
 app.engine('mustache', mustacheExpress());
 
-app.set('view engine', 'mustache');
+app.set('view engine', 'mustache'); //use mustache template to render HTML
 app.set('views', __dirname + '/views');
 
-app.get('/user-directory', function(request,response){
+app.get('/', function(request,response){
 response.render('user-directory', {users : data.users});
 
-});
-app.get('/robot', function(request,response){
-response.render('robot', {users : data.users});
+// });
+// app.get('/robot', function(request,response){
+// response.render('robot', {users : data.users});
 
 });
-app.get('/robot/:username', function(request,response){
-  let person = users.find(function(member){
+app.get('/:username/', function(request,response){
+  let person = data.users.find(function(member){
     return member.username === request.params.username;
-  })
-response.render('robot', person);
-
+  });
+response.render('robot',  {users : person});
+//response.render('robot', {users : data.users})
+// iF we used response.render('robot', person); we'll have to take away the {}#person},{ /person }From the HTML
+//{{>tail}} partials. refers to a file outside the document.
+//{{.}} used to access items in an array.
 });
 
 app.listen(3000,function(){
